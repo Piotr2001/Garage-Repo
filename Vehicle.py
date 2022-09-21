55660b951812108a249172cce41a5a8415398ae2
Piotr Fejdych <piotr.fejdych@sky.uk>
2022-09-21 23:33:53 +0100

    added basic functionality

--- Vehicle.py
+++ Vehicle.py
@@ -18,7 +18,7 @@ def working():
     if V == 1:
         List_Garage()
     elif V == 2:
-        print ("2")
+        add_vehicle()
     elif V == 3:
         print ("3")
     elif V == 4:
@@ -30,35 +30,84 @@ def working():
     else:
         print ("Not a valid input")
 
-working()
-
 def List_Garage():
-    print (Garage)
+    for vehicle in Garage:
+            print(vars(vehicle))
 
-def Add_Vehicle():
+def add_vehicle():
     print ("Adding")
+    A = int(input('''
+    What type of vehicle are you adding?
+    1.Car
+    2.Motorbike
+    3.Van
+    please pick a number:
+    '''))
+    reg = input('Please enter vehicle reg: ')
+    colour = input('Please enter vehicle colour: ')
+    condition = input('Please enter vehicle condition for A to D: ')
+    if A == 1:
+        type = int(input('''
+        Please pick vehicle type:
+        1.Hatch Back
+        2.SUV
+        3.Super Car
+        4.Saloon
+        5.Other
+        Please pick a number: 
+        '''))
+        if type == 1:
+            type = "Hatch Back"
+        elif type == 2:
+            type = "SUV"
+        elif type == 3:
+            type = "Super Car" 
+        elif type == 4:
+            type = "Saloon"
+        elif type == 5:
+            type = "Other"
+        else:
+            print("Not a valid input")
+        no_doors = int(input("Please enter number of doors: "))
+        Car = car(reg, colour, condition, type, no_doors)
+        Garage.append(Car)
+        for vehicle in Garage:
+            print(vars(vehicle))
+    elif A == 2:
+        type = input("Please enter vehicle type: ")
+        Bike = motorbike(reg, colour, condition, type)
+    elif A == 3:
+        type = input("Please enter vehicle type: ")
+        size = input("Please enter vehicle size: ")
+        Van = van(reg, colour, condition, type, size)
+    else:
+        print("Not a valid input")
+
+
     
 class Vehicle:
-    def __init__(self, reg, colour):
+    def __init__(self, reg, colour, condition):
         self.reg = reg
         self.colour = colour
-
-class Motorbike(Vehicle):
-    def __init__(self, reg, colour, type, condition):
-        super().__init__(reg, colour)
-        self.type = type
         self.condition = condition
 
-class Van(Vehicle):
-    def __init__(self, reg, colour, type, condition, size):
-        super().__init__(reg, colour)
+class motorbike(Vehicle):
+    def __init__(self, reg, colour, condition, type):
+        super().__init__(reg, colour, condition)
         self.type = type
-        self.condition = condition
+
+class van(Vehicle):
+    def __init__(self, reg, colour, condition, type, size):
+        super().__init__(reg, colour, condition)
+        self.type = type
         self.size = size
 
 class car(Vehicle):
-    def __init__(self, reg, colour, type, condition, no_doors):
-        super().__init__(reg, colour)
+    def __init__(self, reg, colour, condition, type, no_doors):
+        super().__init__(reg, colour, condition)
         self.type = type
-        self.condition = condition
         self.no_doors = no_doors
+
+First_car = car("abx342", "Red", "D", "SUV", 5)
+Garage = [First_car] 
+working()
